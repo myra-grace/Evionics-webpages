4903188a4a4144bfff3f7f985699c493f31e1951
myra-grace <myrags3@gmail.com>
2022-11-16 03:55:51 -0500

    routs

---
+++ src/js/router.js
@@ -0,0 +1,56 @@
+document.addEventListener("click", (e) => {
+    const { target } = e;
+    if (!target.matches("nav a")) {
+        return;
+    }
+    e.preventDefault();
+    route();
+});
+
+const routes = {
+    404: {
+        template: "/templates/404.html",
+        title: "404",
+        description: "Page not found",
+    },
+    "/": {
+        template: "/templates/index.html",
+        title: "Home",
+        description: "This is the home page",
+    },
+    "/terms-and-conditions": {
+        template: "/templates/terms-and-conditions.html",
+        title: "Evionics - Terms and Conditions",
+        description: "This is the about page",
+    },
+    "/privacy-policy": {
+        template: "/templates/privacy-policy.html",
+        title: "Evionics - Privacy Policy",
+        description: "This is the contact page",
+    },
+};
+
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    locationHandler();
+};
+
+const locationHandler = async () => {
+    const location = window.location.pathname;
+    if (location.length == 0) {
+        location = "/";
+    }
+    const route = routes[location] || routes["404"];
+    const html = await fetch(route.template).then((response) => response.text());
+    document.getElementById("content").innerHTML = html;
+    document.title = route.title;
+    document
+        .querySelector('meta[name="description"]')
+        .setAttribute("content", route.description);
+};
+
+window.onpopstate = locationHandler;
+window.route = route;
+locationHandler();
